608a17c683c6a54db7a48a43a831c60d3778fd0e
NoLogig <max-schweigerdt@web.de>
2018-10-29 08:17:01 +0100

    init

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,59 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'euclid';
+  title = 'Euclid';
+
+  // Rekursive
+  euklid1(a, b) {
+    if (b === 0) {
+      return a;
+    }
+    if (a === 0) {
+      return b;
+    }
+    if (a > b) {
+      return this.euklid1(a - b, b);
+    }
+    return this.euklid1(a, b - a);
+  }
+
+  // Iterative
+  euklid2(a, b) {
+    if (a === 0) {
+      return b;
+    }
+
+    while (b !== 0) {
+      if (a > b) {
+        a -= b;
+        continue;
+      }
+      b -= a;
+    }
+
+    return a;
+  }
+
+  // Modern Variantions
+
+  // Rekursive
+  euklid3(a, b) {
+    if (b === 0) {
+      return a;
+    }
+
+    return this.euklid3(b, a % b);
+  }
+
+  // Iterative
+  euklid4(a, b) {
+    let tmp = 0;
+    while (b !== 0) {
+      tmp = a % b;
+      a = b;
+      b = tmp;
+    }
+    return a;
+  }
+
 }
